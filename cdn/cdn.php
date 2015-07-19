3d642406bb402c7d6ac8888be379cbd7b8680336
Martin Bravo <bravomartin@gmail.com>
2015-07-19 11:56:19 -0400

    protect panel assets from being rewritten to the cdn url

--- cdn/cdn.php
+++ cdn/cdn.php
@@ -15,11 +15,14 @@ if(c::get('cdn.assets')) {
   url::$to = function() use($original) {
 
     $url = call($original, func_get_args());
+  
 
     if(!str::startsWith($url, kirby()->urls()->index())) {
       return $url;
     }
-
+    if(str::contains($url, '/panel/assets')) {
+      return $url;
+    }
     $url = preg_replace_callback('!.*?\/assets\/(.*)$!', function($match) {
       return c::get('cdn.assets') . '/' . $match[1];
     }, $url);
